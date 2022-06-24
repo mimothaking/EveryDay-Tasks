358cb65b69bbead7fb3c2fce149770aed80b9935
Machmachi Mohamed <61361554+mimothaking@users.noreply.github.com>
2022-06-24 17:31:41 +0100

    Working on it

--- todoList/App.js
+++ todoList/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, KeyboardAvoidingView, TextInput, TouchableOpacity } from 'react-native';
 import Task from './components/Task';
 
 export default function App() {
@@ -11,13 +11,23 @@ export default function App() {
 
       <View style={styles.items}>
       {/*This is where tasks will be shown ! */}
-        <Task />
-        <Task />
-        <Task />
-        <Task />  
+        <Task text={'Text one'} />
+        <Task text={'Text two'} />
+      </View>
       </View>
 
-      </View>
+      {/*Add a task*/}
+      <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={styles.writeTaskWrapper}
+      >
+      <TextInput style={styles.input} placeholder={'Write your task here pwese'} />
+      <TouchableOpacity>
+        <View style={styles.addWrapper}>
+          <Text style={styles.addText}>+</Text>
+        </View>
+      </TouchableOpacity>
+      </KeyboardAvoidingView>
 
     </View>
   );
@@ -26,7 +36,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#f55',
+    backgroundColor: '#e9f5f9'
   },
   tasksWrapper : {
     paddingTop: 80,
@@ -34,7 +44,37 @@ const styles = StyleSheet.create({
   },
   sectionTitles: {
     fontSize: 24,
-    fontWeight: 'bold'
+    fontWeight: 'bold',
+    paddingBottom: 35
   },
-  items: {}
+  items: {
+
+  },
+  writeTaskWrapper:{
+    position: 'absolute',
+    bottom: 60,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  input: {
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    backgroundColor: '#fff',
+    borderRadius: 60,
+    borderColor: '#C0C0C0',
+    borderWidth: 1,
+    width: 250
+  },
+  addWrapper:{
+    width: 60,
+    height: 60,
+    backgroundColor: '#fff',
+    borderRadius: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: '#C0C0C0',
+    borderWidth: 1
+  },
 });
--- todoList/components/Task.js
+++ todoList/components/Task.js
@@ -1,16 +1,57 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 
-const Task =() => {
+
+const Task =(props) => {
     return (
-        <View>  
-        <Text>This is a task</Text>
+        <View style={styles.item}>
+        <View style={styles.itemLeft}>
+        <TouchableOpacity style={styles.square}></TouchableOpacity>
+        <Text style={styles.itemText}>{props.text}</Text>
+        </View>
+        <View style={styles.circular}>
+
+        </View>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
+    item:{
+        backgroundColor: '#fff',
+        padding: 15,
+        borderRadius: 10,
+        marginBottom: 15,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        marginBottom: 20
+    },
+    itemLeft:{
+        flexDirection: 'row',
+        alignItems: 'center',
+        flexWrap: 'wrap',
 
+    },
+    square:{
+        width: 24,
+        height: 24,
+        backgroundColor: '#55BCF6',
+        opacity: 0.4,
+        borderRadius: 4
+    },
+    itemText:{
+        paddingLeft: 15,
+        maxWidth: '80%'
+    },
+    circular:{
+        width: 11,
+        height: 11,
+        backgroundColor: '#fff',
+        borderWidth: 2,
+        borderRadius: 10,
+        borderColor: '#55BCF6'
+    }
 });
 
 
